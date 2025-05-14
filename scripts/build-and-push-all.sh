96344c5796b3f80e40353940e77ab2412b788657
Jonas Doumen <jonas.doumen@kuleuven.be>
2025-05-14 14:53:24 +0200

    updating workflows and docker repo

--- scripts/build-and-push-all.sh
+++ scripts/build-and-push-all.sh
@@ -3,8 +3,8 @@
 
 ./scripts/build-all.sh
 
-echo "Will push galahad images with version <$VERSION_LABEL>. Set VERSION_LABEL to override this."
+echo "Will push textlens images with version <$VERSION_LABEL>. Set VERSION_LABEL to override this."
 
-docker push instituutnederlandsetaal/galahad-proxy:$VERSION_LABEL
-docker push instituutnederlandsetaal/galahad-server:$VERSION_LABEL
-docker push instituutnederlandsetaal/galahad-client:$VERSION_LABEL
+docker push cclkuleuven/textlens-proxy:$VERSION_LABEL
+docker push cclkuleuven/textlens-server:$VERSION_LABEL
+docker push cclkuleuven/textlens-client:$VERSION_LABEL
