b5cd878676e093fc4bb715d8593a01438294c619
Jonas Doumen <jonas.doumen@kuleuven.be>
2025-05-16 17:42:44 +0200

    fixing build vars

--- build.sh
+++
@@ -1,3 +0,0 @@
-docker build -t instituutnederlandsetaal/textlens-client:1 client
-docker build -t instituutnederlandsetaal/textlens-proxy:1 proxy
-docker build -t instituutnederlandsetaal/textlens-server:1 server
